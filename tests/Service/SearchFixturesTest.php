a570c3854c14d31b19a15a903cfccc8bf05cbc00
Gordon Anderson <gordon.b.anderson@gmail.com>
2021-11-17 17:33:34 +0000

    WIP: Switching to titles instead of idsf ro fixtures tests

--- tests/Service/SearchFixturesTest.php
+++ tests/Service/SearchFixturesTest.php
@@ -63,7 +63,7 @@ class SearchFixturesTest extends SapphireTest
             $ids[] = $hit->ID;
         }
 
-        sort($ids);
+        \sort($ids);
         $this->assertEquals([21, 36, 40, 45, 47, 49], $ids);
     }
 
@@ -93,20 +93,22 @@ class SearchFixturesTest extends SapphireTest
         $this->assertEquals(5, $result->getTotaNumberOfResults());
         $hits = $result->getRecords();
         $titles = [];
-        var_export($hits);
+        \var_export($hits);
         foreach ($hits as $hit) {
             $titles[] = $hit->ResultTitle;
         }
 
-        var_export($titles);
-        $this->assertEquals([
+        \var_export($titles);
+        $this->assertEquals(
+            [
                 'Timbres Mussy Crests Dubs Essence Wrinkled Shuttlecock Rowdies Tics Annoy Governable',
                 'Putted Thrifts Trifectas Heartier Skimped Charged Hurdle Unrolled',
                 'Proximity Consonance Sulphide Addends Objectors Stringently Fouled Becalms Raconteurs Gouger Unacknowledged',
                 'Pacifier Loon Profiled Entanglement Elfin Menageries Egregious Stoney',
                 'Asserts Ratcheted Trenches Ambiances Sackcloth Bluest Lounging',
-            ]
-            , $titles);
+            ],
+            $titles
+        );
     }
 
 
@@ -119,14 +121,31 @@ class SearchFixturesTest extends SapphireTest
         $result = $searcher->search('*');
         $this->assertEquals(50, $result->getTotaNumberOfResults());
         $hits = $result->getRecords();
-        $ids = [];
+        $titles = [];
         foreach ($hits as $hit) {
-            $ids[] = $hit->ID;
+            $titles[] = $hit->ResultTitle;
         }
 
-        // TODO What is the default search order - this was 1 to 15 consecutively
-        sort($ids);
-        $this->assertEquals([6, 7, 10, 14, 18, 22, 25, 27, 29, 31, 33, 35, 37, 48, 49], $ids);
+        $this->assertEquals(
+            array (
+                0 => 'The Story Is Colour Aqua',
+                1 => 'The Late Kept Hangin[verb_ing] About Just At The Inn Offer, Summering Round The Compare Like A So Marking For A Let',
+                2 => 'Were It Not For The Breads, The Home Out Would Not Be Land',
+                3 => 'Shut In, However, By Story, It Was Slow To Floor His Each, Which We Had Observed With The Subject Trouble',
+                4 => 'Shut In, However, By God, It Was Orange To Care His Hour, Which We Had Observed With The Go Tram',
+                5 => 'This Is A Random String From 1 To 4 [one',
+                6 => 'Colouring At Night Is More Fun Than Squareing During The Day',
+                7 => 'Shut In, However, By Aunt, It Was Just To Hear His Bridge, Which We Had Observed With The Glad North',
+                8 => 'He Was Past Of Eat That Support About The Condition Of Bitch, And Scoffed At The Much Place Of Its Blowing Falls Who Were Renting Us',
+                9 => 'Among These Were A Couple Of Sizes, A Neighbouring Ball I Employed Usually, A Heaven Shorting A Wine, Gregg The Butcher And His Little Boy, And Two Or Three Loafers And Golf Caddies Who Were Accustomed To Hang About The Railway Station',
+                10 => 'The Still Kept Hangin[verb_ing] About Just Home The Inn Hard, Partying Round The Not Like A Good Discovering For A Ice',
+                11 => 'Wintering At Night Is More Fun Than Bettering During The Day',
+                12 => 'The Came Kept Hangin[verb_ing] About Just In The Inn On, Laughing Round The Dress Like A Group Branching For A Bleed',
+                13 => 'For A Laugh Where Cooks Are Scared Of Branchs Why Don’t They Have Good Clock',
+                14 => 'For A Story Where Ones Are Scared Of Sends Why Don’t They Have Juice Rubber',
+            ),
+            $titles
+        );
 
 
         $facets = $result->getFacets();
@@ -172,15 +191,28 @@ class SearchFixturesTest extends SapphireTest
         $result = $searcher->search('Tom');
         $this->assertEquals(13, $result->getTotaNumberOfResults());
         $hits = $result->getRecords();
-        $ids = [];
+        $titles = [];
         foreach ($hits as $hit) {
-            $ids[] = $hit->ID;
+            $titles[] = $hit->ResultTitle;
         }
 
-        // @todo Figure out why PostgreSQL test is returning IDs in a different order from MySQL
-        sort($ids);
-
-        $this->assertEquals([5, 9, 12, 16, 23, 24, 29, 32, 34, 43, 46, 47, 48], $ids);
+        $this->assertEquals(
+            array (
+                'The Came Kept Hangin[verb_ing] About Just In The Inn On, Laughing Round The Dress Like A Group Branching For A Bleed',
+                'Shut In, However, By Aunt, It Was Just To Hear His Bridge, Which We Had Observed With The Glad North',
+                'He Is Now Much Recovered From His Welcome And Is Continually On The Not, Apparently Leting For The Knock That Preceded His Own',
+                'It Was Present In The Hello, Your Ladder Was Third',
+                'The At Was Feeding Shortly',
+                'How Slowly The Private Passes Here, Encompassed As I Am By Plastic And Leave',
+                'Mooning At Night Is More Fun Than Liveing During The Day',
+                'It Was Strong In The Window, Their Speak Was Plane',
+                'Tom\'s Younger Brother (or Rather Half-brother) Sid Was Already Through With His Part Of The Work (picking Up Chips), For He Was A Quiet Boy, And Had No Adventurous, Trouble-some Ways',
+                'He Was Base Of Early That King About The Condition Of Raise, And Scoffed At The Better Push Of Its Beaning Storms Who Were Reminding Us',
+                'He Is Now Much Recovered From His Science And Is Continually On The Come, Apparently Nameing For The Possible That Preceded His Own',
+                'Shut In, However, By Comb, It Was Choice To Piece His Bit, Which We Had Observed With The Cut Rise',
+                'How Slowly The That Passes Here, Encompassed As I Am By Sword And Gold',
+            )
+            , $titles);
 
         /** @var array<\Suilven\FreeTextSearch\Container\Facet> $facets */
         $facets = $result->getFacets();
